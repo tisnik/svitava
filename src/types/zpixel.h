dbb116e88004a13a511e6e68ce941c79133eae11
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-14 09:30:18 +0200

    New data type: ZPixel

---
+++ src/types/zpixel.h
@@ -0,0 +1,14 @@
+/* vim: set fileencoding=utf-8 tabstop=8 noexpandtab */
+
+#ifndef _ZPIXEL_TYPE_H_
+#define _ZPIXEL_TYPE_H_
+
+typedef struct
+{
+	unsigned int iter;
+        double       z_real;
+        double       z_imag;
+} ZPixel;
+
+#endif
+
