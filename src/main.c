507912a38a2dde4b46843d87c5c74c6280d6090b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-29 08:30:05 +0200

    Macros etc.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,35 @@
-int main(int argc, char **argv) {
-        return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "pixmap.h"
+#include "exporters/bmp_writer.h"
+#include "exporters/tga_writer.h"
+#include "renderers/mandelbrot.h"
+
+
+typedef struct
+{
+    int bitmap_width;
+    int bitmap_height;
+} config;
+
+int process_arguments(int argc, char **argv)
+{
+    return 1;
 }
+
+int main(int argc, char **argv)
+{
+    Pixmap *pixmap;
+
+    pixmap = pixmap_create(320, 240, 3);
+    pixmap_clear(pixmap);
+    calculate_mandelbrot(pixmap, 256);
+    bmp_write(pixmap, "test.bmp");
+    pixmap_destroy(pixmap);
+
+    return 0;
+}
+
--- src/types/pixmap.h
+++ src/types/pixmap.h
@@ -1,14 +1,4 @@
-/* vim: set fileencoding=utf-8 tabstop=8 noexpandtab */
-
 #ifndef _pixmap_type_h_
 #define _pixmap_type_h_
-
-typedef struct
-{
-	unsigned int width;
-	unsigned int height;
-	unsigned int bpp;
-	unsigned char *pixels;
-} Pixmap;
 
 #endif
