365cc2654aa3eb0e875b5591ddfd472cd681c75d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-01 11:47:53 +0200

    First implementation of PPM writer

--- src/exporters/ppm_writer.c
+++ src/exporters/ppm_writer.c
@@ -4,7 +4,7 @@
 #include "pixmap.h"
 #include "ppm_writer.h"
 
-void ppm_write_to_stream(const Pixmap *pixmap, FILE *fout)
+void ppm_write_ascii_to_stream(const Pixmap *pixmap, FILE *fout)
 {
     int x, y;
     unsigned char r, g, b;
@@ -21,7 +21,26 @@ void ppm_write_to_stream(const Pixmap *pixmap, FILE *fout)
     }
 }
 
-int ppm_write(const Pixmap *pixmap, const char *file_name)
+void ppm_write_binary_to_stream(const Pixmap *pixmap, FILE *fout)
+{
+    int x, y;
+    unsigned char r, g, b;
+
+    /* header */
+    fprintf(fout, "P6 %d %d 255\n", pixmap->width, pixmap->height);
+
+    /* pixel array */
+    for (y=pixmap->height-1; y>=0; y--) {
+        for (x=0; x<pixmap->width; x++) {
+            pixmap_getpixel(pixmap, x, y, &r, &g, &b);
+            fputc(r, fout);
+            fputc(g, fout);
+            fputc(b, fout);
+        }
+    }
+}
+
+int ppm_write_ascii(const Pixmap *pixmap, const char *file_name)
 {
     FILE *fout;
 
@@ -31,7 +50,26 @@ int ppm_write(const Pixmap *pixmap, const char *file_name)
         return -1;
     }
 
-    ppm_write_to_stream(pixmap, fout);
+    ppm_write_ascii_to_stream(pixmap, fout);
+
+    if (fclose(fout) == EOF)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+int ppm_write_binary(const Pixmap *pixmap, const char *file_name)
+{
+    FILE *fout;
+
+    fout = fopen(file_name, "wb");
+    if (!fout)
+    {
+        return -1;
+    }
+
+    ppm_write_binary_to_stream(pixmap, fout);
 
     if (fclose(fout) == EOF)
     {
--- src/exporters/ppm_writer.h
+++ src/exporters/ppm_writer.h
@@ -4,7 +4,12 @@
 #include <stdio.h>
 #include "pixmap.h"
 
-int ppm_write(const Pixmap * pixmap, const char *file_name);
-void ppm_write_to_stream(const Pixmap * pixmap, FILE * fout);
+/* ASCII PPM with magic number P3 */
+int ppm_write_ascii(const Pixmap * pixmap, const char *file_name);
+void ppm_write_ascii_to_stream(const Pixmap * pixmap, FILE * fout);
+
+/* ASCII PPM with magic number P6 */
+int ppm_write_binary(const Pixmap * pixmap, const char *file_name);
+void ppm_write_binary_to_stream(const Pixmap * pixmap, FILE * fout);
 
 #endif
