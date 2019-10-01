5bb2a05c835b0f0c2e70bf1f0ab42dbb8c7da264
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-01 15:19:54 +0200

    PPM exporter tests

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #include "pixmap.h"
 #include "exporters/bmp_writer.h"
 #include "exporters/tga_writer.h"
+/* #include "exporters/png_writer.h" */
+#include "exporters/ppm_writer.h"
 #include "renderers/mandelbrot.h"
 
 
@@ -28,8 +30,53 @@ int main(int argc, char **argv)
     pixmap_clear(pixmap);
     calculate_mandelbrot(pixmap, 256);
     bmp_write(pixmap, "test.bmp");
+    /*png_write(pixmap, "test.png");*/
+    ppm_write_ascii(pixmap, "test.ppm");
+    ppm_write_binary(pixmap, "test2.ppm");
+    pixmap_destroy(pixmap);
+    /*
+    pixmap = pixmap_create(255, 255, 3);
+    pixmap_clear(pixmap);
+    {
+        int x, y;
+        for (y=0; y<128; y++) {
+            for (x=0; x<128; x++) {
+                pixmap_putpixel(pixmap, x, y, 255, 0, 0);
+            }
+        }
+    }
+    png_write(pixmap, "test1.png");
+    bmp_write(pixmap, "test1.bmp");
     pixmap_destroy(pixmap);
 
+    pixmap = pixmap_create(256, 256, 3);
+    pixmap_clear(pixmap);
+    {
+        int x, y;
+        for (y=0; y<128; y++) {
+            for (x=0; x<128; x++) {
+                pixmap_putpixel(pixmap, x, y, 255, 0, 0);
+            }
+        }
+    }
+    png_write(pixmap, "test2.png");
+    bmp_write(pixmap, "test2.bmp");
+    pixmap_destroy(pixmap);
+
+    pixmap = pixmap_create(257, 257, 3);
+    pixmap_clear(pixmap);
+    {
+        int x, y;
+        for (y=0; y<128; y++) {
+            for (x=0; x<128; x++) {
+                pixmap_putpixel(pixmap, x, y, 255, 0, 0);
+            }
+        }
+    }
+    png_write(pixmap, "test3.png");
+    bmp_write(pixmap, "test3.bmp");
+    pixmap_destroy(pixmap);
+    */
     return 0;
 }
 
