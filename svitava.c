3befad0aac8bada3c223763ffa1f585db498338b
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-02 10:29:32 +0100

    Image_t structure and it's constructor

--- svitava.c
+++ svitava.c
@@ -22,3 +22,79 @@ build as executable:
     gcc -lm -o svitava svitava.c
 */
 
+/* Image types */
+#define GRAYSCALE 1
+#define RGB 3
+#define RGBA 4
+
+
+/**
+ * Structure that represents raster image of configurable resolution and bits
+ * per pixel format.
+ */
+typedef struct {
+    unsigned int   width;
+    unsigned int   height;
+    unsigned int   bpp;
+    unsigned char *pixels;
+} image_t;
+
+
+
+/**
+ * Compute the total size in bytes of an image's pixel buffer.
+ *
+ * @param image Pointer to the image whose buffer size will be computed.
+ *
+ * @returns Total number of bytes required for the image's pixel buffer
+ *          (width * height * bpp).
+ */
+size_t image_size(const image_t *image) {
+    if (image == NULL) {
+        return 0;
+    }
+    return image->width * image->height * image->bpp;
+}
+
+
+
+/**
+ * Create an image_t with the given width, height, and bytes-per-pixel,
+ * allocating a pixel buffer.
+ *
+ * The returned image_t fields width, height, and bpp are initialized and
+ * pixels points to a newly allocated buffer of size width * height * bpp. If
+ * allocation fails, pixels will be NULL.
+ *
+ * @param width  Image width specified in pixels.
+ * @param height Image height specified in pixels.
+ * @param bpp    Bytes per pixel (bytes used to store a single pixel).
+ *
+ * @returns The initialized image_t; its `pixels` member points to the
+ *          allocated buffer or NULL on allocation failure.
+ */
+image_t image_create(const unsigned int width, const unsigned int height, const unsigned int bpp) {
+    image_t image;
+    /* validate parameters */
+    if (width == 0 || height == 0 || (bpp != GRAYSCALE && bpp != RGB && bpp != RGBA)) {
+        image.width = 0;
+        image.height = 0;
+        image.bpp = 0;
+        image.pixels = NULL;
+        return image;
+    }
+    image.width = width;
+    image.height = height;
+    image.bpp = bpp;
+
+    /* callers must check that image.pixels != NULL */
+    image.pixels = (unsigned char *)malloc(image_size(&image));
+
+    /* make sure the image will be 'zero value' when pixels are not allocated */
+    if (image.pixels == NULL) {
+        image.width = 0;
+        image.height = 0;
+        image.bpp = 0;
+    }
+    return image;
+}
