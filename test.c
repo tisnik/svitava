260d77cfc7a4679f518e53781fc278ec5adb8d70
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-07 09:56:46 +0100

    New unit test: test_filter_smooth_3x3_block_grayscale_image

--- test.c
+++ test.c
@@ -1836,6 +1836,29 @@ void test_filter_smooth_3x3_block_rgba_image(void) {
 }
 
 /**
+ * Test filter on grayscale image.
+ */
+void test_filter_smooth_3x3_block_grayscale_image(void) {
+    TEST_BEGIN
+    image_t image = image_create(5, 5, GRAYSCALE);
+    assert(image.pixels != NULL);
+    image_clear(&image);
+
+    /* Set center pixel to white */
+    image_putpixel(&image, 2, 2, 255, 255, 255, 255);
+
+    filter_smooth_3x3_block(&image);
+
+    /* Center pixel should be smoothed */
+    unsigned char r, g, b, a;
+    image_getpixel(&image, 2, 2, &r, &g, &b, &a);
+    assert(r == 28); /* (255*1 + 0*8) / 9 = 28 */
+
+    free(image.pixels);
+    TEST_END
+}
+
+/**
  * Run the complete image processing test suite in a deterministic order.
  *
  * Executes all unit tests covering image size, creation, cloning, clearing,
@@ -1950,5 +1973,6 @@ int main(void) {
     test_filter_smooth_3x3_block_image_without_pixels();
     test_filter_smooth_3x3_block_rgb_image();
     test_filter_smooth_3x3_block_rgba_image();
+    test_filter_smooth_3x3_block_grayscale_image();
     return 0;
 }
