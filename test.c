1dc26774d9d74ca16c9e7c55273ca03e0f38b9ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-19 16:20:45 +0100

    New unit test test_filter_vertical_edge_detection_3x3_rgb_image

--- test.c
+++ test.c
@@ -2327,6 +2327,22 @@ void test_filter_vertical_edge_detection_3x3_image_without_pixels(void) {
 }
 
 /**
+ * Test filter_vertical_edge_detection_3x3 doesn't crash.
+ */
+void test_filter_vertical_edge_detection_3x3_rgb_image(void) {
+    TEST_BEGIN
+    image_t image = image_create(5, 5, RGB);
+    assert(image.pixels != NULL);
+    image_clear(&image);
+
+    filter_vertical_edge_detection_3x3(&image);
+
+    /* Should complete without crashing */
+    free(image.pixels);
+    TEST_END
+}
+
+/**
  * Run the complete image processing test suite in a deterministic order.
  *
  * Executes all unit tests covering image size, creation, cloning, clearing,
@@ -2474,6 +2490,7 @@ int main(void) {
 
     test_filter_vertical_edge_detection_3x3_null_image();
     test_filter_vertical_edge_detection_3x3_image_without_pixels();
+    test_filter_vertical_edge_detection_3x3_rgb_image();
 
     return 0;
 }
