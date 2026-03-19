418693291ce49b72703fb390075dc37fb2b68ce5
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-03-19 16:18:38 +0100

    New unit test test_filter_vertical_edge_detection_3x3_null_image

--- test.c
+++ test.c
@@ -2300,6 +2300,16 @@ void test_filter_horizontal_edge_detection_3x3_rgb_image(void) {
 }
 
 /**
+ * Ensure filter_vertical_edge_detection_3x3 safely handles a NULL image without crashing.
+ */
+void test_filter_vertical_edge_detection_3x3_null_image(void) {
+    TEST_BEGIN
+    /* Should not crash */
+    filter_vertical_edge_detection_3x3(NULL);
+    TEST_END
+}
+
+/**
  * Run the complete image processing test suite in a deterministic order.
  *
  * Executes all unit tests covering image size, creation, cloning, clearing,
@@ -2444,5 +2454,7 @@ int main(void) {
     test_filter_horizontal_edge_detection_3x3_null_image();
     test_filter_horizontal_edge_detection_3x3_image_without_pixels();
     test_filter_horizontal_edge_detection_3x3_rgb_image();
+
+    test_filter_vertical_edge_detection_3x3_null_image();
     return 0;
 }
